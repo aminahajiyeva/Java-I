6655de619c76c667833abf85266563a530ffc02a
amina <aminahajiyeva67@gmail.com>
2025-03-23 16:38:57 -0400

    Add files via upload

---
+++ Lab 8/Counter.java
@@ -0,0 +1,136 @@
+/**
+ * A simple counter that tracks the count between a minimum and maximum value.
+ * This counter may be incremented, decremented and/or reset to its default minimum value.
+ *
+ * @author (Amina Hajiyeva, 101303729)
+ * @version (March 1 2025)
+ */
+public abstract class Counter
+{
+    /** The current value of this counter. */
+    private int count;
+
+    /** The minimum value of this counter. */
+    private int minimumCount;
+
+    /** The maximum value of this counter. */
+    private int maximumCount;
+
+    /** The default minimum value of this counter. */
+    private static final int DEFAULT_MINIMUM = 0;
+
+    /** The default maximum value of this counter. */
+    private static final int DEFAULT_MAXIMUM = 999;
+
+    /**
+     * Constructs a new Counter whose current count is
+     * initialized to DEFAULT_MINIMUM, and which counts between
+     * DEFAULT_MINIMUM and DEFAULT_MAXIMUM, inclusive.
+     */
+    public Counter()
+    {
+        minimumCount = DEFAULT_MINIMUM;
+        maximumCount = DEFAULT_MAXIMUM;
+        count = minimumCount;
+    }
+
+    /**
+     * Constructs a new Counter whose current count is
+     * initialized to minCount, and which counts between
+     * minCount and maxCount, inclusive.
+     * 
+     * @param minCount The minimum value for the counter.
+     * @param maxCount The maximum value for the counter.
+     */
+    public Counter(int minCount, int maxCount)
+    {
+        minimumCount = minCount;
+        maximumCount = maxCount;
+        count = minimumCount;
+    }
+
+    /**
+     * Returns the maximum value of this counter.
+     * 
+     * @return The maximum count value.
+     */
+    public int maximumCount()
+    {
+        return maximumCount;
+    }
+
+    /**
+     * Returns the minimum value of this counter.
+     * 
+     * @return The minimum count value.
+     */
+    public int minimumCount()
+    {
+        return minimumCount;
+    }
+
+    /**
+     * Returns this counter's current count.
+     * 
+     * @return The current count.
+     */
+    public int count()
+    {
+        return count;
+    }
+
+    /**
+     * Returns true if this counter is at its minimum value.
+     * 
+     * @return True if count is at minimum value, false otherwise.
+     */
+    public boolean isAtMinimum()
+    {
+         return (count == minimumCount);
+    }
+
+    /**
+     * Returns true if this counter is at its maximum value.
+     * 
+     * @return True if count is at maximum value, false otherwise.
+     */
+    public boolean isAtMaximum()
+    {
+        return (count == maximumCount);
+    }
+
+    /**
+     * Resets this counter to its minimum value.
+     */
+    public void reset()
+    {
+        count = minimumCount;
+    }
+    
+    /**
+     * Increment this counter by 1.
+     */
+    public void countUp()
+    {
+        count++;
+    }
+    
+    /**
+     * Decrement this counter by 1.
+     */
+    public void decrementCount()
+    {
+        count--;
+    }
+    
+    /**
+     * Sets the counter to its maximum value.
+     */
+    public void setToMaximum()
+    {
+        count = maximumCount;
+    }
+    
+    public abstract void countDown();
+
+}
---
+++ Lab 8/CounterTest.java
@@ -0,0 +1,72 @@
+/**
+ * The test class CounterTest.
+ *
+ * @author  (Amina Hajiyeva 101303729)
+ * @version (March 6 2025)
+ */
+public class CounterTest extends junit.framework.TestCase
+{
+    private Counter c1;
+    
+    /**
+     * Default constructor for test class CounterTest
+     */
+    public CounterTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    protected void setUp()
+    {
+        c1 = new RollOverCounter(1, 10);        
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    protected void tearDown()
+    {
+    }
+    
+    /**
+     * Tests the original (lab 8) RollOverCounter methods.
+     */
+    public void testAllRollOverCounterMethods()
+    {    
+        /* Verify that the counter is in the correct initial state. */
+        assertEquals(1, c1.minimumCount());
+        assertEquals(10, c1.maximumCount());
+        assertEquals(1, c1.count());        
+        
+        assertTrue(c1.isAtMinimum());
+        assertFalse(c1.isAtMaximum());
+  
+        /* Count 1 -> 2 */
+        
+        c1.countUp();
+        assertEquals(2, c1.count());
+        assertFalse(c1.isAtMinimum());
+        
+        /* Count 3, 4, ...9, 10 */
+        for (int i = 1; i < 9; i++) {
+            c1.countUp();
+        }
+        assertTrue(c1.isAtMaximum());
+        
+        /* Count 10 -> 1 */
+        c1.countUp();
+        assertEquals(1, c1.count());
+        
+        /* Verify that reset works. */
+        c1.countUp();
+        c1.countUp();
+        c1.reset();
+        assertEquals(1, c1.count());
+    }    
+}
