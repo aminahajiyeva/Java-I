1ce9ec33d78c8303d4f349833a0d06b0e6b8c077
amina <aminahajiyeva67@gmail.com>
2025-03-23 16:24:27 -0400

    Add files via upload

---
+++ Lab 4/Train.java
@@ -0,0 +1,31 @@
+/**
+ * Train is a passenger train which has cars filled with seats filled with passengers
+ *
+ * @author Lynn Marshall, SCE
+ * @version 1.1 September 23, 2012
+ * 
+ * Amina Hajiyeva
+ */
+public class Train
+{
+    
+    public Train(int numberOfCars)
+    {
+
+    }
+   
+    public void addCar(Car car)
+    {
+
+    }
+   
+    public boolean issueTicket(boolean requestedClass)
+    {
+        return false;
+    }
+   
+    public boolean cancelTicket(int id, int seatNo)
+    {
+        return false;
+    }
+}
